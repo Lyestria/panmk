a6147ee7349a15d8286fe3269432058098b84579
kiritofeng <rogerjfu@hotmail.com>
2019-04-06 10:36:12 -0400

    Modify batch file

--- panmk.bat
+++ panmk.bat
@@ -1,5 +1,5 @@
-Rem Execute latexmk.pl, searching for it in the PATH
-Rem Assume perl is in the PATH.  If not, you should replace 'perl' by 
-Rem the full pathname of the perl executable, e.g., 'C:\perl\bin\perl.exe'
+Rem Execute panmk.py, searching for it in the PATH
+Rem Assume python is in the PATH.  If not, you should replace 'python' by 
+Rem the full pathname of the python executable.
 
-perl -S panmk.pl %1 %2 %3 %4 %5 %6 %7 %8 %9
+python panmk.py %1 %2 %3 %4 %5 %6 %7 %8 %9
