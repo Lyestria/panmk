c9b99809c7f485961484cb1be01a0c54ffb8b7dc
Roger Fu <kiritofeng@users.noreply.github.com>
2019-09-03 11:22:01 -0400

    Windows is silly

--- panmk.bat
+++
@@ -1,5 +0,0 @@
-Rem Execute panmk.py, searching for it in the PATH
-Rem Assume python is in the PATH.  If not, you should replace 'python' by 
-Rem the full pathname of the python executable.
-
-python panmk.py %1 %2 %3 %4 %5 %6 %7 %8 %9
